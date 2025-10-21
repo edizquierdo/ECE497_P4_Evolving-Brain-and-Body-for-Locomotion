789ed4784f0fe25576ff9d6b8a5abf4c3725bda7
Eduardo J. Izquierdo <edizquierdo@gmail.com>
2025-10-21 08:36:33 -0400

    Add files via upload

---
+++ Oct20classdemo/generate.py
@@ -0,0 +1,22 @@
+import pyrosim.pyrosim as pyrosim
+
+
+def World(n,xnum,ynum):
+    pyrosim.Start_SDF("boxes.sdf")
+    for x in range(xnum):
+        for y in range(ynum):
+            for i in range(n):
+                s = 1 - i/n
+                pyrosim.Send_Cube(name="Box", pos=[x,y,0.5 + i] , size=[s,s,s])
+    pyrosim.End()
+
+def Robot():
+    pyrosim.Start_URDF("robot.urdf")
+    pyrosim.Send_Cube(name="0", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name="0_1", parent="0", child="1", type = "revolute", position=[0.5,0,1])
+    pyrosim.Send_Cube(name="1", pos=[0.5,0,0.5] , size=[1,1,1])
+    pyrosim.End()
+
+Robot()
+
+#World(10,5,5)
